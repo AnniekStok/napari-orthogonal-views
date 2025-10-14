d8b784449a0b94aec760c8eacf75db2138b1973c
AnniekStok <anniek.stokkermans@gmail.com>
2025-10-14 14:49:09 +0200

    add test for camera syncing

--- src/napari_orthogonal_views/_tests/test_ortho_view_manager.py
+++ src/napari_orthogonal_views/_tests/test_ortho_view_manager.py
@@ -9,6 +9,8 @@ from napari_orthogonal_views.ortho_view_widget import OrthoViewWidget
 
 
 def test_orthoview_manager(make_napari_viewer, qtbot):
+    """Test initialization of the ortho view manager and its show/hide functions."""
+
     viewer = make_napari_viewer()
     m = _get_manager(viewer)
     show_orthogonal_views(viewer)
@@ -17,4 +19,35 @@ def test_orthoview_manager(make_napari_viewer, qtbot):
     hide_orthogonal_views(viewer)
     qtbot.waitUntil(lambda: not m.is_shown(), timeout=1000)
     assert isinstance(m.right_widget, QWidget)
+    m.cleanup()
+
+
+def test_sync_camera(make_napari_viewer, qtbot):
+    """Test synchronization of the camera events between the orthoviews."""
+
+    viewer = make_napari_viewer()
+    m = _get_manager(viewer)
+    show_orthogonal_views(viewer)
+    qtbot.waitUntil(lambda: m.is_shown(), timeout=1000)
+    assert isinstance(m.right_widget, OrthoViewWidget)
+
+    # Test zoom sync
+    m.set_zoom_sync(True)
+    w = m.right_widget
+    zoom_emitter = m.viewer.camera.events.zoom
+
+    # Check if any (emitter, handler) in _connections has this emitter
+    assert any(em == zoom_emitter for em, _ in w._connections)
+
+    # Check if the connection is removed
+    m.set_zoom_sync(False)
+    assert not any(em == zoom_emitter for em, _ in w._connections)
+
+    # Test center sync
+    m.set_center_sync(True)
+    center_emitter = m.viewer.camera.events.center
+    assert any(em == center_emitter for em, _ in w._connections)
+    m.set_center_sync(False)
+    assert not any(em == center_emitter for em, _ in w._connections)
+
     m.cleanup()
